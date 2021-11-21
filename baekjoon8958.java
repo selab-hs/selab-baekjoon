6c624af970ed1e16874265bf482989c2cacfb994
yunsup <zksdbstjq@gmail.com>
2021-11-21 11:24:48 +0900

    Refactor : baekjoon No.8958

---
+++ baekjoon8958.java
@@ -0,0 +1,45 @@
+package SeLab.Solution_2021_11_15;
+
+import java.util.Scanner;
+
+public class baekjoon8958 {
+    public static final int ZERO = 0;
+    public static final String REGEX = "";
+    public static final String CORRECT = "O";
+    public static final String WRONG = "X";
+
+    public static int gradeTestCase(String[] TestCase) {
+        int score = ZERO, testCaseTotalScore = ZERO;
+        for (String answer : TestCase){
+            switch (answer) {
+                case CORRECT: {
+                    score++;
+                    testCaseTotalScore += score;
+                    break;
+                }
+                case WRONG: {
+                    score = ZERO;
+                }
+            }
+        }
+        return testCaseTotalScore;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int testCaseNum = scanner.nextInt();
+        int[] testCaseResult = new int[testCaseNum];
+
+        for (int i = 0; i < testCaseNum; i++){
+            String[] TestCase = scanner.next().split(REGEX);
+            testCaseResult[i] = gradeTestCase(TestCase);
+        }
+        scanner.close();
+
+        for (int result : testCaseResult){
+            System.out.println(result);
+        }
+
+    }
+}
