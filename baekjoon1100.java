1c5e12b8d1a2ed151c9a1c7c248a66ba7ce1303c
yunsup <zksdbstjq@gmail.com>
2021-11-28 11:20:55 +0900

    Refactor: 코드 리팩터링 매직넘버 상수화 / 변수 명칭 재정의 / scanner 닫음

--- baekjoon1100.java
+++ baekjoon1100.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 public class baekjoon1110 {
     public final static String[][] CHESSBOARD = new String[8][8];
     public final static int ZERO = 0;
-    public final static String REGEX = "";
+    public final static int INIT_RANGE = 0;
+    public final static int DIVISOR = 2;
     public final static int BOARDSIZE = 8;
+    public final static String REGEX = "";
+    public final static PIECE  = "F";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = ZERO; i < BOARDSIZE; i++){
+        for (int i = INIT_RANGE; i < BOARDSIZE; i++){
             CHESSBOARD[i] = divideTheSpace(scanner.next(), CHESSBOARD, i);
         }
+        scanner.close();
         System.out.println(onTheWhiteBoard(CHESSBOARD));
     }
 
     public static String[] divideTheSpace(String col, String[][] CHESSBOARD, int i) {
-        for (int j = ZERO; j < BOARDSIZE; j++){
+        for (int j = INIT_RANGE; j < BOARDSIZE; j++){
             String[] str = col.split(REGEX);
             CHESSBOARD[i][j] = str[j];
         }
@@ -25,8 +29,8 @@ public class baekjoon1110 {
 
     public static int onTheWhiteBoard(String[][] CHESSBOARD) {
         int pieceOnTheWhiteBoard = ZERO;
-        for (int i = ZERO; i < BOARDSIZE; i++){
-            for (int j = ZERO; j < BOARDSIZE; j++){
+        for (int i = INIT_RANGE; i < BOARDSIZE; i++){
+            for (int j = INIT_RANGE; j < BOARDSIZE; j++){
                 if (((isEven(i) && isEven(j)) || (isOdd(i) && isOdd(j))) &&  isF(CHESSBOARD[i][j])) {
                     pieceOnTheWhiteBoard++;
                 }
@@ -35,14 +39,14 @@ public class baekjoon1110 {
         return pieceOnTheWhiteBoard;
     }
     public static boolean isEven(int num) {
-        return num % 2 == ZERO;
+        return num % DIVISOR == ZERO;
     }
 
     public static boolean isOdd(int num) {
-        return num % 2 != ZERO;
+        return num % DIVISOR != ZERO;
     }
 
     public static boolean isF(String piece) {
-        return piece.equals("F");
+        return piece.equals(PIECE);
     }
 }
