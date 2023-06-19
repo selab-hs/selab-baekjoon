cb458e77b3ed4a532f967f6b35bc90201400cead
KimKiHyun0206 <rlarlgus0206@naver.com>
2023-06-19 14:08:23 +0900

    update : 서울에서 김서방 찾기

--- programmers/Main.java
+++ programmers/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        String kim = "Kim";
+        String[] arr = {"Jane","Kim"};
+        int answer = 0;
+        for (int i=0;i< arr.length;i++){
+            if(arr[i].equals(kim)){
+                answer =i;
+                break;
+            }
+        }
+        System.out.println(answer);
     }
-
 }
